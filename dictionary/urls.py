74b522cdd99079667824a0019b441d935dde01cc
kuzmenko-ms <kumaks09@yandex.ru>
2014-05-07 13:05:15 +0400

    variant_add

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from dictionary.views import varvar
+from dictionary.views import varvar,add_var
 
  
 urlpatterns = patterns('',
  
 url(r'^var/$',varvar, name='varvar'),
+url(r'^addvar/$',add_var, name='add_var'),
    
 )
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,7 +1,7 @@
 from django.template import RequestContext
 from polls.models import Project,Variants
-from polls.models import Pvar_var
-from polls.models import Component_type
+from polls.models import Pvar_var,Pvar1_var2
+from polls.models import Component_type,Components
 from django.views.generic import ListView, DetailView
 from django.forms import ModelForm
 from django.shortcuts import redirect
@@ -33,3 +33,28 @@ def varvar(request):
         return render(request, 'polls/var.html', {'errors': errors, 'form':form, 'model_p_vv' : model_p_vv,'model_var' : model_var })
     return render(request, 'polls/var.html', {'errors': errors, 'model_p_vv' : model_p_vv,'model_var' : model_var })
 
+def add_var(request):
+    errors = []
+    form = {}
+    model_vr = Component_type.objects.all()
+    model_p = Pvar1_var2.objects.all()
+    if request.POST:
+        form['project_var'] = request.POST.get('project_var')
+        form['name_var'] = request.POST.get('name_var')
+        form['type_comp'] = request.POST.get('type_comp')
+        form['p'] = request.POST.get('p')
+        if not form['name_var']:
+            errors.append('error name')
+        if not form['type_comp']:
+            errors.append('error name')
+        if not form['p']:
+            errors.append('error name')        
+        if not errors:
+           
+            model1 = Variants(project_id = '53',type_c_id=form['type_comp'],num=form['name_var'],pvar_id=form['p'])
+            model1.save()
+       
+        return render(request, 'polls/var_add.html', {'errors': errors, 'form':form,'model_vr' : model_vr,'model_p' : model_p })
+    return render(request, 'polls/var_add.html', {'errors': errors,'model_vr' : model_vr,'model_p' : model_p })
+
+    
